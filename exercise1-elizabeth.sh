8253f7c99cf4e07b317d0d09d31844b432f37695
EC2 Default User <ec2-user@ip-172-16-182-83.ec2.internal>
2020-09-24 16:14:47 +0000

    Added a script.

---
+++ exercise1-elizabeth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Author: Elizabeth Park
+# The script takes as input one argument, a directory, and outputs the number of different permission types in this directory.
+
+ls -l $1 | cut -d ' ' -f 1 | tail --lines=+2 | uniq | wc -l
